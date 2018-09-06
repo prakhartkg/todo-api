43edb0d7cfa597827b786280ced596da142fa32f
Prakhar Jain <prakhartkg@gmail.com>
2018-09-06 10:39:10 +0530

    update delete

--- playground/mongodbUpdate.js
+++ playground/mongodbUpdate.js
@@ -7,12 +7,21 @@ MongoClient.connect('mongodb://localhost:27017',(err,client)=>{
   }else {
     console.log("mongodb server connected");
     const db = client.db('Todo').collection('Todo')
-          .findOneAndDelete({
-            _id: new ObjectID('5b8e7d17adb3fe13f26e8a47')
-          }).then((result)=>{
+          .findOneAndUpdate({
+            _id: new ObjectID('5b8fe31d1b4e2d145f3b7006')
+          },
+          {
+            $set:{
+              completed:true
+            }
+          },
+          {
+            returnOriginal:false
+          }
+        ).then((result)=>{
             console.log(JSON.stringify(result,undefined,2));
           },(err)=>{
             console.log("Unable to fetch List");
-          })
+          });
   }
 });
