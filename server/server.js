b6659bf50720000aefe3228b6fdbd536bd6ad2f5
Prakhar Jain <prakhartkg@gmail.com>
2018-09-08 23:38:20 +0530

    get todos

--- server/server.js
+++ server/server.js
@@ -24,6 +24,13 @@ app.post('/todos',(req,res)=>{
 
 });
 
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  },(err)=>{
+    res.status(404).send(err);
+  });
+});
 
 app.listen(3000,()=>{
   console.log('Started on 3000');
