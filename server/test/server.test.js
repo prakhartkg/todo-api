117dc29d642965fb760fef88100c05ca8c715d92
Prakhar Jain <prakhartkg@gmail.com>
2018-09-10 23:46:03 +0530

    test cases for todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo')
 
 const todoList =[{
+  _id:new ObjectID(),
   text:"First test"
 },{
+  _id:new ObjectID(),
   text:"second todo"
 }];
 
@@ -67,6 +70,32 @@ describe('GET /todos',()=>{
       .expect((res)=>{
         expect(res.body.todos.length).toBe(2);
       })
+      .end(done);
+  });
+});
+
+describe('GET /todos/:id',()=>{
+  it('should return todo',(done)=>{
+    request(app)
+      .get(`/todos/${todoList[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo.text).toBe(todoList[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404',(done)=>{
+    request(app)
+      .get(`/todos/${new ObjectID()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 400',(done)=>{
+    request(app)
+      .get(`/todos/${new ObjectID()}+1`)
+      .expect(400)
       .end(done);
   });
 });
